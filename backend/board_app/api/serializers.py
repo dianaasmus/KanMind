3f90cf9451459b098464e0f0261542d988975439
Diana Asmus <121640648+dianaasmus@users.noreply.github.com>
2025-03-27 13:37:26 +0100

    chore: add Board fields

--- backend/board_app/api/serializers.py
+++ backend/board_app/api/serializers.py
@@ -3,9 +3,22 @@ from board_app.models import Board, Member, Task
 
 
 class BoardSerializer(serializers.ModelSerializer):
+    member_count = serializers.SerializerMethodField()
+    ticket_count = serializers.SerializerMethodField()
+    owner_id = serializers.SerializerMethodField()
+
     class Meta:
         model = Board
-        fields = "__all__"
+        fields = ["id", "title", "member_count", "ticket_count", "owner_id"]
+
+    def get_member_count(self, obj):
+        return obj.members.count()
+
+    def get_ticket_count(self, obj):
+        return obj.tasks.count()
+
+    def get_owner_id(self, obj):
+        return obj.owner.id
 
 
 class MemberSerializer(serializers.ModelSerializer):
