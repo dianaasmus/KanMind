669fd6420a70855c4f50c92cf1b6bc258de998d2
Diana Asmus <121640648+dianaasmus@users.noreply.github.com>
2025-04-02 20:52:40 +0200

    chore (frontend): change registration/login logic

--- backend/KanMind/urls.py
+++ backend/KanMind/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("board_app.api.urls")),
     path("api-auth/", include("rest_framework.urls")),
-    path("api/auth/", include("user_auth_app.api.urls")),
+    path("api/", include("user_auth_app.api.urls")),
 ]
